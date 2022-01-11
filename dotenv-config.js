9419268d57e7c8ab60ef3b64d00d2c752671bd6b
philz-stizles <theophilusighalo@gmail.com>
2022-01-11 20:11:41 +0100

    modify dotenv ci example

--- dotenv-config.js
+++ dotenv-config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv-safe');
 
 dotenv.config({
   example: process.env.CI ? '.env.ci.example' : '.env.example',
-  allowEmptyValues: true
+  // allowEmptyValues: true
 });
 
 module.exports = dotenv;
